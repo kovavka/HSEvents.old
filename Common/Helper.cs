04b71fccb3563e1add554f625cfa64b646829286
gikap <kovavka@gmail.com>
2018-03-10 16:33:02 +0500

    temp

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,18 @@ namespace Common
         public static IEnumerable<T> AsEnumerable<T>(this T self)
         {
             yield return self;
+        }
+
+        public static string GetDescription(this Enum source)
+        {
+            FieldInfo fi = source.GetType().GetField(source.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0) return attributes[0].Description;
+
+            return source.ToString();
         }
     }
 }
--- HSEvents.Web/Controllers/DirectoryController.cs
+++ HSEvents.Web/Controllers/DirectoryController.cs
@@ -1,23 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Common;
+using Domain;
+using Domain.Events;
+using Infrastructure.Repositories;
 
 namespace HSEvents.Web.Views
 {
     public class DirectoryController : Controller
     {
-        
-
-        public ActionResult School()
+        public ActionResult Index(DirectoryType? type)
         {
-            return View();
+            var view = View();
+            if (type == null)
+                return view;
+
+            var methodInfo = GetType().GetMethod(type.ToString());
+
+            if (methodInfo==null)
+                return view;
+
+            ViewBag.Title = type.GetDescription();
+            ViewData["list"] = (IEnumerable<string>) methodInfo.Invoke(this, null);
+
+            return view;
         }
 
-        public ActionResult Index(string type)
+        private IEnumerable<string> Attendees()
         {
-            return View();
+            var repo = new NHRepository<Attendee>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.ContactInfo.FullName);
         }
+        private IEnumerable<string> ContactPersons()
+        {
+            var repo = new NHRepository<ContactPerson>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.ContactInfo.FullName);
+        }
+        private IEnumerable<string> Schools()
+        {
+            var repo = new NHRepository<School>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> AcademicPrograms()
+        {
+            var repo = new NHRepository<AcademicProgram>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> SchoolTypes()
+        {
+            var repo = new NHRepository<SchoolType>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> CityTypes()
+        {
+            var repo = new NHRepository<CityType>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> Volunteers()
+        {
+            var repo = new NHRepository<Volunteer>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.FullName);
+        }
+        private IEnumerable<string> Groups()
+        {
+            var repo = new NHRepository<School>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> Departments()
+        {
+            var repo = new NHRepository<Department>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+        private IEnumerable<string> Users()
+        {
+            var repo = new NHRepository<User>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.ContactInfo.FullName);
+        }
+        private IEnumerable<string> Employees()
+        {
+            var repo = new NHRepository<School>();
+            var data = repo.GetAll();
+
+            return data.Select(x => x.Name);
+        }
+    }
+
+    public enum DirectoryType
+    {
+        [Description("Участники")] Attendees,
+        [Description("Контактные лица школ")] ContactPersons,
+        [Description("Школы")] Schools,
+        [Description("Академические программы")] AcademicPrograms,
+        [Description("Типы школ")] SchoolTypes,
+        [Description("Типы населенных пунктов")] CityTypes,
+        [Description("Волонтеры")] Volunteers,
+        [Description("Группы")] Groups,
+        [Description("Департаменты")] Departments,
+        [Description("Пользователи")] Users,
+        [Description("Сотрудники")] Employees,
     }
 }
