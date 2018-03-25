535aaf4f6d5ec26c1c3b194f8ebcc127f4f22d34
gikap <kovavka@gmail.com>
2018-03-25 15:49:56 +0500

    удалила лишнее

--- HSEvents.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace HSEvents.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
