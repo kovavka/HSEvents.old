7aab3f369eb5bf3eed0e88d14053a820d19cf648
gkapger <gkapger@persis.ru>
2018-03-27 04:31:12 +0500

    revert

--- Infrastructure/NHibernateHelper.cs
+++ Infrastructure/NHibernateHelper.cs
@@ -24,7 +24,7 @@ namespace Infrastructure
         {
             ISessionFactory sessionFactory = Fluently.Configure()
                 .Database(MsSqlConfiguration.MsSql2012.ConnectionString(
-                        @"Server=.\SQLExpress; Initial Catalog=HSEvents; Integrated Security=SSPI;")
+                        @"Server=.; Initial Catalog=HSEvents; Integrated Security=SSPI;")
                     .ShowSql()
                 )
                 .Mappings(m => m.FluentMappings.Conventions.AddFromAssemblyOf<EnumConvention>())
